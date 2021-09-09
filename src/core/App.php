0a1cd3cbdf63faead0b047277c0e4043df9e5d36
BONIFACE <contact@yohannboniface.dev>
2021-09-09 10:50:18 +0200

    ✨ Adding basic logging

--- src/core/App.php
+++ src/core/App.php
@@ -3,16 +3,23 @@
 namespace vehicles\core;
 
 use Faker\Factory;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use vehicles\classes\Car;
 use vehicles\classes\Truck;
 
 class App
 {
     private $faker;
+    private $logger;
 
     public function __construct()
     {
         $this->faker = Factory::create();
+        $this->logger = new Logger('Logger');
+        $this->logger->pushHandler(
+            new StreamHandler(ROOT_PATH . '/runtime/vehicles.log', Logger::DEBUG)
+        );
     }
 
     function get_id_plate(): string
@@ -22,11 +29,13 @@ class App
 
     function run()
     {
+        $this->logger->debug("Generating a car...");
         $car = new Car($this->get_id_plate());
         $car->start();
 
         echo '<br>';
 
+        $this->logger->debug("Generating a truck...");
         $truck = new Truck($this->get_id_plate());
         $truck->start();
     }
